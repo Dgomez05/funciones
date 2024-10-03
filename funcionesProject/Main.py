8458f560e1ae6092b5c4843bc48b5fc048de3b3d
Duban Gomez <dubangomez596@gmail.com>
2024-10-03 14:51:43 -0500

    se crea el proyecto con eñ archivo main y una funcion con el hola mundo

---
+++ funcionesProject/Main.py
@@ -0,0 +1,15 @@
+#definicion de una funcion
+def my_function():
+    print("Hello from a function")
+
+print("Es hoy es hoy es hoy EL QUIZ")
+
+#Invocar la funcion
+my_function()
+
+#funcion que recibe argumentos
+def mostrarNombre(nombre):
+    print("Su nombre es " + nombre)
+
+nom = input("Ingrese su nombre: ")
+mostrarNombre(nom)
